739a233285c6122863d31801b65a30d77eb41f53
hiroyuki0402 <88645049+hiroyuki0402@users.noreply.github.com>
2022-02-15 23:37:39 +0900

    [add] viewModel作成

--- Hiro42Kadai15/AppDelegate.swift
+++ Hiro42Kadai15/AppDelegate.swift
@@ -9,8 +9,19 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var navigationController: UINavigationController?
+
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let firstViewController: AddFruitsViewController = AddFruitsViewController() 
+        navigationController = UINavigationController(rootViewController: firstViewController)
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
--- Hiro42Kadai15/View/AddFruitsView.swift
+++ Hiro42Kadai15/View/AddFruitsView.swift
@@ -10,9 +10,35 @@ import UIKit
 
 
 class AddFruitsView: UIView {
+    private var fruitsField: UITextField!
+    weak var delegate: AddFruitsViewDelegate?
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fruitsField = UITextField()
+        fruitsField?.delegate = self
+        addSubview(fruitsField)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        fruitsField?.frame = CGRect(x: bounds.origin.x + 30,
+                                    y: bounds.origin.y + 30,
+                                    width: bounds.size.width - 40,
+                                    height: 50)
+
+    }
+}
+
+extension AddFruitsView: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        delegate?.createView(fruitsAdd: self, fruitsName: fruitsField?.text ?? "")
+        return true
+    }
 }
 
 protocol AddFruitsViewDelegate: AnyObject {
-    
+    func createView(fruitsAdd view: AddFruitsView, fruitsName: String)
+    func createView(saveDidtap view: AddFruitsView)
+    func createView(backDidtap view: AddFruitsView)
 }
+
